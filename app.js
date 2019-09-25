308ea9cabed44652b601557f0ea1d8427463294b
Thais Silveira <thais_lsilveira@hotmail.com>
2019-09-25 15:00:51 -0300

    Routes and Templates

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const mysql = require('mysql');
 const handlebars = require('express-handlebars');
 const app = express();
 
+//Template engine
+app.engine("handlebars", handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+//Routes and Templates
+app.get("/:id?", function(req, res){
+   /*res.sendFile(__dirname+"/index.html"); 
+   console.log(req.params.id);*/
+   res.render('index', {id:req.params.id});
+});
 
 //Start server
 app.listen(3000, function(req, res){
